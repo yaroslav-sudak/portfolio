c7972a8b86648bd3f9521887b4a2241567cb7d74
Yaroslav Sudak <25privetik2016@gmail.com>
2025-05-08 00:31:05 +0300

    Update tooltip inputs

--- src/app/components/shared/tooltip/tooltip.component.ts
+++ src/app/components/shared/tooltip/tooltip.component.ts
@@ -3,14 +3,9 @@ import { Component, input } from '@angular/core';
 @Component({
   selector: 'app-tooltip',
   templateUrl: './tooltip.component.html',
-  styleUrls: ['./tooltip.component.css']
+  styleUrls: ['./tooltip.component.css'],
 })
 export class TooltipComponent {
-  params = input<{
-    position?: "top" | "bottom" | "left" | "right",
-    text: string
-  }>({
-    position: "bottom",
-    text: ""
-  });
+  position = input<'top' | 'bottom' | 'left' | 'right'>('bottom');
+  text = input<string>('Tooltip example');
 }
